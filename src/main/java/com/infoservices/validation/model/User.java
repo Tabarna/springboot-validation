d2eadba7a2e5c94c0bfd780cd126871bcc8bb07f
Tabarna <abarnana1@gmail.com>
2022-03-17 20:38:48 +0530

    First commit

---
+++ src/main/java/com/infoservices/validation/model/User.java
@@ -0,0 +1,15 @@
+package com.infoservices.validation.model;
+
+
+import javax.persistence.Entity;
+
+@Entit
+public class User {
+
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+    private int mobile;
+
+}
